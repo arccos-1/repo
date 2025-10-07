3a5f0721e1c6730ee35ae3da1c0c8fcb80b89cb6
TimofeyPolyakov <timofejpolakov42@gmail.com>
2025-10-07 16:04:24 +0300

    commitsdkjsdkl

--- dart_application_1/bin/dart_application_1.dart
+++ dart_application_1/bin/dart_application_1.dart
@@ -1,5 +1,8 @@
-import 'package:dart_application_1/dart_application_1.dart' as dart_application_1;
+import 'dart:io';
 
-void main(List<String> arguments) {
-  print('Hello world: ${dart_application_1.calculate()}!');
+void main() {
+  String? line = stdin.readLineSync();
+  print(line);
+  print(line);
+  print(line);
 }
